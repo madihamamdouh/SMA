6c86538a0eca6e167749b922c6fbdc4a8d3e9c1a
madihamamdouh <69203448+madihamamdouh@users.noreply.github.com>
2026-05-29 20:43:51 +0400

    feature: add fonts golbally

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,8 +8,11 @@ export default function App() {
     const SafeAreaView = styled(RNSafeAreaView);
     return (
         <SafeAreaView className="flex-1 p-5 bg-background">
-            <Text className="text-xl font-bold text-success">
-                Welcome to Nativewind!
+            <Text className="text-4xl font-bold text-success">
+                Home
+            </Text>
+            <Text className="text-4xl font-sans-extrabold text-success">
+                Home
             </Text>
             <Link href="/onboarding" className="mt-4 rounded-md text-xl font-bold bg-primary p-4 text-white">
                 Go to onboarding</Link>
@@ -18,17 +21,7 @@ export default function App() {
             <Link href="/(auth)/sign-up" className="mt-4 rounded-md text-xl font-bold bg-primary p-4 text-white">
                 Go to sign up</Link>
 
-            <Link href={{
-                pathname: "/subscriptions/[id]",
-                params: {id :"claude"}
-            }} className="mt-4 rounded-md text-xl font-bold bg-primary p-4 text-white">
-                Claude Subscription </Link>
 
-            <Link href={{
-                pathname: "/subscriptions/[id]",
-                params: {id :"codex"}
-            }} className="mt-4 rounded-md text-xl font-bold bg-primary p-4 text-white">
-                Claude codeX </Link>
 
         </SafeAreaView>
     );
