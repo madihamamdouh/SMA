dc5925442a7d2030cc7c7fa7704ffdc0c492fc76
madihamamdouh <69203448+madihamamdouh@users.noreply.github.com>
2026-05-31 16:20:15 +0400

    Revert "feature: add fonts golbally"
    This reverts commit 6c86538a0eca6e167749b922c6fbdc4a8d3e9c1a.
    
    # Conflicts:
    #	app/(tabs)/index.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,5 @@
-import {SplashScreen, Stack} from "expo-router";
+import { Stack } from "expo-router";
 import '@/global.css';
-import {useFonts} from "expo-font";
-import {useEffect} from "react";
 export default function RootLayout() {
-  const [fontsLoad] = useFonts({
-    'sans-regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
-    'sans-bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
-    'sans-medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
-    'sans-semibold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
-    'sans-extrabold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
-    'sans-light': require('../assets/fonts/PlusJakartaSans-Light.ttf')
-
-  })
-
-  useEffect( ()=>{
-    if(fontsLoad){
-      SplashScreen.hideAsync();
-    }
-  },[fontsLoad])
-  if(!fontsLoad) return null;
   return <Stack screenOptions={{headerShown: false}} />;
 }
