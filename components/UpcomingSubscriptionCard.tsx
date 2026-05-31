56e3c3ced2a3b1fdb12850b7ecacfda930cbb9ec
madihamamdouh <69203448+madihamamdouh@users.noreply.github.com>
2026-05-31 15:15:04 +0400

    Initial commit
    Generated by create-expo-app 4.0.0.

---
+++ components/UpcomingSubscriptionCard.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const UpcomingSubscriptionCard = () => {
+    return (
+        <View>
+            <Text>UpcomingSubscriptionCard</Text>
+        </View>
+    )
+}
+export default UpcomingSubscriptionCard
