bc1b6156cb1ef05140387a2e8a0127872ba8b0f8
madihamamdouh <69203448+madihamamdouh@users.noreply.github.com>
2026-08-03 20:04:12 +0400

    delete app.josn,and move  config to app.config.js

--- app.config.js
+++ app.config.js
@@ -1,11 +1,88 @@
-const { expo } = require('./app.json')
 
 module.exports = {
-  expo: {
-    ...expo,
-    extra: {
-      posthogProjectToken: process.env.POSTHOG_PROJECT_TOKEN,
-      posthogHost: process.env.POSTHOG_HOST,
-    },
-  },
+     expo: {
+          name: "SMA",
+          slug: "sma-app",
+          version: "1.0.0",
+          orientation: "portrait",
+          icon: "./assets/icons/logo.png",
+          scheme: "reactnativeapp",
+          userInterfaceStyle: "automatic",
+          newArchEnabled: true,
+          owner: "madiha-m",
+          android: {
+               package: "com.madiha.sma",
+               adaptiveIcon: {
+                    backgroundColor: "#E6F4FE",
+                    foregroundImage: "./assets/images/android-icon-foreground.png",
+                    backgroundImage: "./assets/images/android-icon-background.png",
+                    monochromeImage: "./assets/images/android-icon-monochrome.png"
+               },
+               "edgeToEdgeEnabled": true,
+               "predictiveBackGestureEnabled": false
+          },
+          ios: {
+               bundleIdentifier: "com.madiha.sma",
+               supportsTablet: true
+          },
+          web: {
+               output: "static",
+               favicon: "./assets/images/favicon.png"
+          },
+          extra: {
+               posthogProjectToken: process.env.POSTHOG_PROJECT_TOKEN,
+               posthogHost: process.env.POSTHOG_HOST,
+               eas: {
+                    projectId: "1e75acde-881d-41e5-853a-621c5e52da4e"
+               },
+          },
+          plugins: [
+               "expo-router",
+               [
+                    "expo-splash-screen",
+                    {
+                         "image": "./assets/images/splash-pattern.png",
+                         "imageWidth": 200,
+                         "resizeMode": "contain",
+                         "backgroundColor": "#ffffff",
+                         "dark": {
+                              "backgroundColor": "#000000"
+                         },
+                    },
+               ],
+               [
+                    "expo-font",
+                    {
+                         "fonts": [
+                              "./assets/fonts/PlusJakartaSans-Regular.ttf",
+                              "./assets/fonts/PlusJakartaSans-Bold.ttf",
+                              "./assets/fonts/PlusJakartaSans-Medium.ttf",
+                              "./assets/fonts/PlusJakartaSans-SemiBold.ttf",
+                              "./assets/fonts/PlusJakartaSans-ExtraBold.ttf",
+                              "./assets/fonts/PlusJakartaSans-Light.ttf"
+                         ],
+                    },
+               ],
+
+               [
+                    "expo-build-properties",
+                    {
+                         android: {
+                              packagingOptions: {
+                                   exclude: ["META-INF/versions/9/OSGI-INF/MANIFEST.MF"]
+                              },
+                         },
+                    },
+               ],[
+
+                    "expo-localization"
+               ]
+
+          ],
+          "experiments": {
+               "typedRoutes": true,
+               "reactCompiler": true
+          }
+     },
 }
+
