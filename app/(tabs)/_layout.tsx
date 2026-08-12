aa43924d6b37371c7516da59be89f96cd1e4f562
madihamamdouh <69203448+madihamamdouh@users.noreply.github.com>
2026-08-12 20:11:37 +0400

    update the tabIcon function

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,6 +6,14 @@ import { Redirect, Tabs } from "expo-router";
 import { Image, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
+const TabIcon = ({ focused, icon }: TabIconProps) => (
+  <View className="tabs-icon">
+    <View className={clsx("tabs-pill", focused && "tabs-active")}>
+      <Image source={icon} resizeMode="contain" className="tabs-glyph" />
+    </View>
+  </View>
+);
+
 const TabLayout = () => {
   const insets = useSafeAreaInsets();
   const tabBar = components.tabBar;
@@ -18,15 +26,7 @@ const TabLayout = () => {
   if (!isSignedIn) {
     return <Redirect href="/(auth)/sign-in" />;
   }
-  const TabIcon = ({ focused, icon }: TabIconProps) => {
-    return (
-      <View className="tabs-icon">
-        <View className={clsx("tabs-pill", focused && "tabs-active")}>
-          <Image source={icon} resizeMode="contain" className="tabs-glyph" />
-        </View>
-      </View>
-    );
-  };
+
   return (
     <Tabs
       screenOptions={{
